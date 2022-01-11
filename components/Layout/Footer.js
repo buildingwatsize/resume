f2f37567255e96233beb53bd9bfab7f39482c896
Chinnawat Chimdee <chinnawat.cpre@gmail.com>
2022-01-11 14:36:07 +0700

    [Wat][Initialized] - Init app

---
+++ components/Layout/Footer.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      Made with ❤️ by <Link href={'https://github.com/buildingwatsize'}>Watsize</Link>
+    </footer>
+  )
+}
+
+export default Footer

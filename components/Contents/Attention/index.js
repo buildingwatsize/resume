943b1e1361eeff8c7ece8a839789ed2359664449
Chinnawat Chimdee <chinnawat.cpre@gmail.com>
2022-06-29 09:06:47 +0700

    [Wat][Added] - Spotify Playlist iframe (TBD for securities)

--- components/Contents/Attention/index.js
+++ components/Contents/Attention/index.js
@@ -3,9 +3,13 @@ import TopicLayout from 'components/Layout/TopicLayout'
 const Attention = () => {
   return (
     <TopicLayout id="attention" title='What we are the same?'>
-
+      <SpotifyPlaylist />
     </TopicLayout>
   )
+}
+
+const SpotifyPlaylist = () => {
+  return <iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/playlist/37i9dQZF1EUMDoJuT8yJsl?utm_source=generator" width="100%" height="380" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" />
 }
 
 export default Attention

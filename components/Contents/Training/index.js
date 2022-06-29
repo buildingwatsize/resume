8d11128881bc398542c6bbc43e5336c9ac53429e
Chinnawat Chimdee <chinnawat.cpre@gmail.com>
2022-06-29 11:07:33 +0700

    [Wat][Added] - Scroll Handler on Training Section - Footer shape divider as a gimmick

--- components/Contents/Training/index.js
+++ components/Contents/Training/index.js
@@ -1,4 +1,5 @@
-import TopicLayout from "components/Layout/TopicLayout"
+import TopicLayout from "components/Layout/TopicLayout";
+import { useState } from "react";
 
 const courseList = [
   {
@@ -38,10 +39,18 @@ const courseList = [
     organize: "Thai Programmer Association, Software Park Thailand",
   },
 ]
+
 const Training = () => {
+  const [scroll, setScroll] = useState(false)
+  const scrollHandler = ({ target: { scrollLeft } }) => { setScroll(scrollLeft > 10) }
   return (
     <TopicLayout id="training" title='Training'>
-      <ol className="flex overflow-x-scroll items-start p-4 bg-medium-blue/20 rounded-md px-4 sm:px-8 shadow-md">
+      <ol onScroll={scrollHandler} className="relative flex overflow-x-scroll items-start p-4 bg-medium-blue/20 rounded-md px-4 sm:px-8 shadow-md">
+        {!scroll && <div className="absolute right-8 bottom-[10%] -rotate-90">
+          <svg xmlns="http://www.w3.org/2000/svg" className="animate-bounce h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
+          </svg>
+        </div>}
         {courseList.map(el => {
           return (
             <li key={el.key} className="relative mb-6 pl-1">

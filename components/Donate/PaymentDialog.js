eb9df5402a74c8f7c0c249936dcd1979ea2bb6cd
Chinnawat Chimdee <chinnawat.cpre@gmail.com>
2023-10-06 13:24:31 +0700

    [Wat][Added] - Dialog Alert on result when got response from omise

--- components/Donate/PaymentDialog.js
+++ components/Donate/PaymentDialog.js
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { NumericFormat } from "react-number-format";
 import Axios from "utils/axios.service";
 
+import Alert from "components/Alert/Alert";
 import Button from "components/Button/Button";
+import Dialog from "components/Dialog/Dialog";
 import Row from "components/Flex/Row";
+import CheckIcon from "components/Utils/CheckIcon";
+import InfoIcon from "components/Utils/InfoIcon";
 import {
   API_PATH,
   MINIMUM_AMOUNT,
@@ -34,6 +38,7 @@ const withOmise = (Component) => {
 
 const PaymentDialog = ({ omise, loading, onCancel }) => {
   const [processing, setProcessing] = useState(false);
+  const [processedData, setProcessedData] = useState(null);
   const [amount, setAmount] = useState(null);
 
   const handleClick = () => {
@@ -64,6 +69,7 @@ const PaymentDialog = ({ omise, loading, onCancel }) => {
   };
 
   const handleClickCancel = () => {
+    setProcessedData(null);
     setProcessing(false);
     setAmount(null);
     if (onCancel) onCancel();
@@ -80,58 +86,98 @@ const PaymentDialog = ({ omise, loading, onCancel }) => {
     try {
       let response = await Axios.post(API_PATH.CHARGE, payload);
       console.log({ response });
-      setAmount(null);
-      alert("Success");
+      setProcessedData({
+        // isOk: true,
+        icon: <CheckIcon />,
+        title: "🎉 สำเร็จ 🎉",
+        content: `รายการจำนวน ${amount} THB ถูกดำเนินการแล้ว สามารถตรวจสอบผลการทำรายการ ได้ตามช่องทางที่บัตรฯ ระบุไว้`,
+      });
     } catch (error) {
       console.error(error);
-      alert("Failed");
+      setProcessedData({
+        // isOk: false,
+        icon: <InfoIcon />,
+        title: "เกิดข้อผิดพลาด",
+        content: `รายการจำนวน ${amount} THB จะยังไม่ถูกดำเนินการ สามารถตรวจสอบผลการทำรายการ ได้ตามช่องทางที่บัตรฯ ระบุไว้`,
+      });
     }
   };
 
+  const getAlert = useMemo(
+    () => (
+      <Alert
+        icon={processedData?.icon}
+        title={<div className="text-xl mt-4">{processedData?.title}</div>}
+        content={processedData?.content}
+        action={
+          <div className="mt-4">
+            <Button onClick={handleClickCancel}>ปิด</Button>
+          </div>
+        }
+      />
+    ),
+    [processedData]
+  );
+
   return (
-    <Row className={"mx-6 flex justify-center"}>
-      <div className="flex flex-col w-[30%] min-w-[350px] bg-bold-blue/40 p-8 rounded-md">
-        <div className="mb-6 bg-danger/70 text-center rounded-md">
-          BETA TEST
-        </div>
-        <div className="mb-8">
-          <div className="text-lg mb-2">ช่องทางชำระเงินที่รองรับ:</div>
-          <div className="text-white/70">
-            {PAYMENT_METHOD_LABEL_LIST.join(", ")}
+    <>
+      <Row className={"mx-6 flex justify-center"}>
+        <div className="flex flex-col w-[30%] min-w-[350px] bg-bold-blue/40 p-8 rounded-md">
+          <div className="mb-6 bg-danger/70 text-center rounded-md">
+            BETA TEST
+          </div>
+          <div className="mb-8">
+            <div className="text-lg mb-2">ช่องทางชำระเงินที่รองรับ:</div>
+            <div className="text-white/70">
+              {PAYMENT_METHOD_LABEL_LIST.join(", ")}
+            </div>
+          </div>
+          <div className="mb-8">
+            <div className="text-lg">ยอดเงิน (THB)</div>
+            <NumericFormat
+              value={amount}
+              onValueChange={({ value }) => {
+                setAmount(value);
+              }}
+              thousandSeparator=","
+              allowNegative={false}
+              decimalScale={2}
+              fixedDecimalScale={true}
+              placeholder={MINIMUM_AMOUNT}
+              customInput={Input}
+            />
+            <div className="text-sm text-white/70 p-1">
+              ({minimumAmountDescription})
+            </div>
+          </div>
+          <div className="flex justify-between gap-6 items-center">
+            <div
+              onClick={handleClickCancel}
+              className="underline cursor-pointer"
+            >
+              ยกเลิก
+            </div>
+            <Button
+              onClick={handleClick}
+              disabled={loading || processing}
+              className="w-full"
+            >
+              สร้างรายการ
+            </Button>
           </div>
         </div>
-        <div className="mb-8">
-          <div className="text-lg">ยอดเงิน (THB)</div>
-          <NumericFormat
-            value={amount}
-            onValueChange={({ value }) => {
-              setAmount(value);
-            }}
-            thousandSeparator=","
-            allowNegative={false}
-            decimalScale={2}
-            fixedDecimalScale={true}
-            placeholder={MINIMUM_AMOUNT}
-            customInput={Input}
-          />
-          <div className="text-sm text-white/70 p-1">
-            ({minimumAmountDescription})
-          </div>
+      </Row>
+
+      <Dialog visible={loading || processing} loading={true}>
+        {processing ? "Processing..." : "Loading..."}
+      </Dialog>
+
+      <Dialog visible={!!processedData}>
+        <div className="relative top-20 mx-auto p-5 border-0 w-[80vw] max-w-lg sm:w-2/3 sm:max-w-sm shadow-lg rounded-md bg-medium-blue">
+          {getAlert}
         </div>
-        <div className="flex justify-between gap-6 items-center">
-          <div onClick={handleClickCancel} className="underline cursor-pointer">
-            ยกเลิก
-          </div>
-          <Button
-            onClick={handleClick}
-            disabled={loading || processing}
-            className="w-full"
-          >
-            สร้างรายการ
-          </Button>
-        </div>
-      </div>
-    </Row>
+      </Dialog>
+    </>
   );
 };
 
